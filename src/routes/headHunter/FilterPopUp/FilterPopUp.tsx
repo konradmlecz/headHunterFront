778fc9c4f9f7ce5da93b79a1b49c9e17f3db1322
Przemek-Szadkowski <przemoszadkowski@o2.pl>
2022-07-31 14:15:32 +0200

    feat: add styles to next segments

--- src/routes/headHunter/FilterPopUp/FilterPopUp.tsx
+++ src/routes/headHunter/FilterPopUp/FilterPopUp.tsx
@@ -20,8 +20,8 @@ export const FilterPopUp = ({setIsPopUpVisible}: Props) => {
             <div className="popup-inner-wrapper">
                 <form action="" className="popup-inner-wrapper__form">
                     <div className="popup-inner-wrapper__form__top-segment">
-                        <p>Filtrowanie</p>
-                        <button>Wyczyść wszystkie</button>
+                        <p className="popup-inner-wrapper__form__top-segment__title">Filtrowanie</p>
+                        <button className="popup-inner-wrapper__form__top-segment__button">Wyczyść wszystkie</button>
                     </div>
                     <div className="popup-inner-wrapper__form__course-rate-segment">
                         <p className="popup-inner-wrapper__form__course-rate-segment__title">Ocena przejścia kursu</p>
@@ -125,43 +125,47 @@ export const FilterPopUp = ({setIsPopUpVisible}: Props) => {
                     </div>
                     <div className="popup-inner-wrapper__form__place-segment">
                         <p className="popup-inner-wrapper__form__place-segment__title">Preferowane miejsce pracy</p>
-                        <label htmlFor="remote">Praca zdalna</label>
                         <input type="checkbox" id="remote" name="job-place" value="Praca zdalna"/>
-                        <label htmlFor="office">Praca w biurze</label>
+                        <label htmlFor="remote">Praca zdalna</label>
                         <input type="checkbox" id="office" name="job-place" value="Praca w biurze"/>
+                        <label htmlFor="office">Praca w biurze</label>
                     </div>
                     <div className="popup-inner-wrapper__form__contract-segment">
                         <p className="popup-inner-wrapper__form__contract-segment__title">Oczekiwany tryb kontraktu</p>
-                        <label htmlFor="employment">Umowa o pracę</label>
                         <input type="checkbox" id="employment" name="job-contract" value="Umowa o pracę"/>
-                        <label htmlFor="b2b">B2B</label>
+                        <label htmlFor="employment">Umowa o pracę</label>
                         <input type="checkbox" id="b2b" name="job-contract" value="B2B"/>
-                        <label htmlFor="mandate">Umowa zlecenie</label>
+                        <label htmlFor="b2b">B2B</label>
                         <input type="checkbox" id="mandate" name="job-contract" value="Umowa zlecenie"/>
-                        <label htmlFor="contract">Umowa o dzieło</label>
+                        <label htmlFor="mandate">Umowa zlecenie</label>
                         <input type="checkbox" id="contract" name="job-contract" value="Umowa o dzieło"/>
+                        <label htmlFor="contract">Umowa o dzieło</label>
                     </div>
                     <div className="popup-inner-wrapper__form__salary-segment">
                         <p className="popup-inner-wrapper__form__salary-segment__title">Oczekiwane wynagrodzenie miesięczne</p>
                         <label htmlFor="from">Od</label>
-                        <input type="number" id="from" name="job-salary"/>
+                        <input type="number" id="from" name="job-salary" placeholder="zł"/>
                         <label htmlFor="to">Do</label>
-                        <input type="number" id="to" name="job-salary"/>
+                        <input type="number" id="to" name="job-salary" placeholder="zł"/>
                     </div>
                     <div className="popup-inner-wrapper__form__intern-segment">
                         <p className="popup-inner-wrapper__form__intern-segment__title">Zgoda na odbycie bezpłatnych praktyk/stażu na początek</p>
-                        <label htmlFor="intern-yes">Tak</label>
-                        <input type="radio" id="intern-yes" name="internship" value="yes"/>
-                        <label htmlFor="intern-no">Nie</label>
-                        <input type="radio" id="intern-no" name="internship" value="no"/>
+                        <div className="popup-inner-wrapper__form__intern-segment__radio-inputs">
+                            <label htmlFor="intern-yes">
+                            <input className="popup-inner-wrapper__form__intern-segment__radio" type="radio" id="intern-yes" name="internship" value="yes"/>
+                            Tak</label>
+                            <label htmlFor="intern-no">
+                            <input className="popup-inner-wrapper__form__intern-segment__radio" type="radio" id="intern-no" name="internship" value="no"/>
+                            Nie</label>
+                        </div>
                     </div>
                     <div className="popup-inner-wrapper__form__experience-segment">
                         <p className="popup-inner-wrapper__form__experience-segment__title">Ilość miesięcy doświadczenia komercyjnego kandydata w programowaniu</p>
                         <input type="number"/>
                     </div>
                     <div className="popup-inner-wrapper__form__bottom-segment">
-                        <button className="popup-inner-wrapper__form__cancel-button" onClick={handleCancelButton}>Anuluj</button>
-                        <input type="submit" value="Pokaż wyniki"/>
+                        <button className="popup-inner-wrapper__form__bottom-segment__cancel-button" onClick={handleCancelButton}>Anuluj</button>
+                        <input className="popup-inner-wrapper__form__bottom-segment__submit-button" type="submit" value="Pokaż wyniki"/>
                     </div>
                 </form>
             </div>
