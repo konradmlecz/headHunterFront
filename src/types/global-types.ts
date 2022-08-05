1f59e89cc2291364704f9fcae10f22c71e73ff6f
adam kujawski <a.kujawski90@gmail.com>
2022-08-05 22:52:42 +0200

    - added global-types

---
+++ src/types/global-types.ts
@@ -0,0 +1,26 @@
+export enum UserRole {
+    ADMIN = 'admin',
+    HR = 'hr',
+    STUDENT = 'student',
+}
+
+export enum StudentStatus {
+    AVAILABLE = 'available',
+    INTERVIEW = 'interview',
+    EMPLOYED = 'employed',
+}
+
+export enum expectedTypeWork {
+    ONSITE = 'Na miejscu',
+    RELOCATION = 'Gotowość do przeprowadzki',
+    REMOTELY = 'Wyłącznie zdalnie',
+    HEBRID = 'Hybrydowo',
+    NOMETTER = 'Bez znaczenia',
+}
+
+export enum expectedContractType {
+    UOP = 'Tylko UoP',
+    B2B = 'Możliwe B2B',
+    UZ = 'Możliwe UZ/UoD',
+    NO = 'Brak',
+}
