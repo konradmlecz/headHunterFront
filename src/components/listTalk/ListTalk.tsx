4fb79da6d2bc39102dbdc3561e18e6a680ff4ca4
KonradMleczkoJMK <98386964+KonradMleczkoJMK@users.noreply.github.com>
2022-08-08 20:41:38 +0200

    set eslint

--- src/components/listTalk/ListTalk.tsx
+++ src/components/listTalk/ListTalk.tsx
@@ -1,19 +1,21 @@
-import React,{FC, useEffect} from 'react';
-import {Box, Typography ,TextField ,Button} from '@mui/material';
-import {Global} from '../../context/store';
-import {getData} from '../../utils/getData';
+import React, { FC, useEffect } from 'react';
+import { Box, Typography, TextField, Button } from '@mui/material';
+import { Global } from '../../context/store';
+import { getData } from '../../utils/getData';
 import ListTalkItem from '../listTalkItem/ListTalkItem';
 function ListTalk() {
-
     const { dispatchGlobalContext, globalState } = React.useContext(Global);
-      return (
-        <Box sx={{
-          background:'#292A2B'
-        }}>
-          {globalState.studentsInteview.map(item=> <ListTalkItem key={item.email} itemStudent={item}/>)}
+    return (
+        <Box
+            sx={{
+                background: '#292A2B',
+            }}
+        >
+            {globalState.studentsInteview.map((item) => (
+                <ListTalkItem key={item.email} itemStudent={item} />
+            ))}
         </Box>
-      );
-    }
+    );
+}
 
-    export default ListTalk
-    
+export default ListTalk;
