60bee68288c684d994e866b17b25105ca6ae6f88
Rafal Zw <zwierz.rafal@gmail.com>
2022-09-17 17:59:59 +0200

    Updated RemainderPassword view.

--- src/routes/ reminderPassword/reminderPassword.tsx
+++ src/routes/ reminderPassword/reminderPassword.tsx
@@ -1,6 +1,7 @@
 import React, {FormEvent, useState} from "react";
 import {adress} from "../../constant/setting";
 import logo from "../../images/megaK_logo.png";
+import './reminderPassword.css';
 
 
 export const ReminderPassword = () => {
@@ -29,34 +30,36 @@ export const ReminderPassword = () => {
         }
     };
 
-    return(
-        <div className="settings" style={{height: "100vh"} }>
-            <div className="settingsWrapper">
-                <div className="settingsTitle">
-                    <form action="" className="settingsForm" onSubmit={handleSubmit}>
-                        <img
-                            src={logo}
-                            alt="MegaK logo"
-                            className="login-wrapper__form__logo"
-                        />
-                        <label>Email</label>
-                        <input
-                            type="email"
-                            required={true}
-                            // value={email}
-                            onChange={e => setEmail({...email, email: e.target.value})}
-                        />
-                        <button className="btn settingsSubmit" type="submit">Wyślij</button>
-                        {success &&
-                            <span style={{textAlign: "center", color: "green", marginTop: "10px"}}>Na podany email wysłano link do ustawienia nowego hasła...</span>}
-                        {error &&
-                            <span style={{
-                                color: "darkred",
-                                marginTop: "10px"
-                            }}>Coś poszło nie tak... Spóbuj ponownie.</span>}
-                    </form>
+    return (
+        <div className="reminder-wrapper">
+            <form action="" className="reminder-wrapper__form" onSubmit={handleSubmit}>
+                <img
+                    src={logo}
+                    alt="MegaK logo"
+                    className="login-wrapper__form__logo"
+                />
+                <label>Email</label>
+                <input
+                    className="reminder-wrapper__form__input-email"
+                    type="email"
+                    required={true}
+                    onChange={e => setEmail({...email, email: e.target.value})}
+                />
+                <div className="reminder-wrapper__form__bottom-form-line">
+                    <button className="reminder-wrapper__form__submit-button" type="submit">Wyślij</button>
+                    {success &&
+                        <span style={{
+                            textAlign: "center",
+                            color: "green",
+                            marginTop: "10px"
+                        }}>Na podany email wysłano link do ustawienia nowego hasła...</span>}
+                    {error &&
+                        <span style={{
+                            color: "darkred",
+                            marginTop: "10px"
+                        }}>Coś poszło nie tak... Spóbuj ponownie.</span>}
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
